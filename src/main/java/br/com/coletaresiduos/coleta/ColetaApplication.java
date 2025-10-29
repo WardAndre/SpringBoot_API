f73eab037c0935247dc2a63778f3261b26e63fb1
Andre Ward <andre.ward62@gmail.com>
2025-10-29 18:17:05 -0300

    Project created

---
+++ src/main/java/br/com/coletaresiduos/coleta/ColetaApplication.java
@@ -0,0 +1,13 @@
+package br.com.coletaresiduos.coleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColetaApplication.class, args);
+	}
+
+}
